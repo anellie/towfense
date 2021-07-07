504338e6d1d81746e3a27bea27adc8ec4a7b9714
Ellie Ang <git@angm.xyz>
2021-07-07 02:12:23 +0200

    Map animation

--- core/src/xyz/angm/towfense/level/WorldMap.kt
+++ core/src/xyz/angm/towfense/level/WorldMap.kt
@@ -1,6 +1,6 @@
 /*
  * Developed as part of the towfense project.
- * This file was last modified at 7/7/21, 2:07 AM.
+ * This file was last modified at 7/7/21, 2:12 AM.
  * Copyright 2020, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
@@ -10,8 +10,10 @@ package xyz.angm.towfense.level
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Interpolation
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Group
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.utils.Align
 import xyz.angm.towfense.IntVector
@@ -22,15 +24,17 @@ class WorldMap private constructor(val path: Path) : Group() {
     fun drawBackground() {
         val pathImage = Assets.tex("map/path")
         val location = path.start.cpy()
+        var delay = 0.1f
         for (s in path.segments) {
             for (i in 0..s[LEN]) {
-                plotPoint(pathImage, location, s[DIR])
+                plotPoint(pathImage, location, s[DIR], delay)
                 Direction.add(location, s[DIR], 1)
+                delay += 0.1f
             }
         }
     }
 
-    private fun plotPoint(tex: Texture, loc: IntVector, dir: Int) {
+    private fun plotPoint(tex: Texture, loc: IntVector, dir: Int, delay: Float) {
         val tileSize = IntVector(Gdx.graphics.width, Gdx.graphics.height).div(tex.width, tex.height)
         val texPos = loc.cpy().mul(tileSize)
 
@@ -40,6 +44,18 @@ class WorldMap private constructor(val path: Path) : Group() {
         actor.x = texPos.x.toFloat()
         actor.y = texPos.y.toFloat()
         addActor(actor)
+
+        actor.scaleX = 0f
+        actor.scaleY = 0f
+        actor.addAction(
+            Actions.sequence(
+                Actions.delay(delay),
+                Actions.parallel(
+                    Actions.scaleTo(1f, 1f, 0.5f, Interpolation.pow3),
+                    Actions.rotateBy(360f, 0.5f, Interpolation.pow2)
+                )
+            )
+        )
     }
 
     companion object {
